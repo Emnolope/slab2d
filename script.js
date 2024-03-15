f5b37de891a7815a66fed446c411c9cbbe78d3c7
Emnolope <emmanuelnlopez+15@gmail.com>
2024-03-15 18:30:14 +0000

    Fixed the jumpy big textarea bug

--- script.js
+++ script.js
@@ -301,10 +301,69 @@ function addNoteToSearchResults(noteLines, hidden, lines) {
   // Adjust the textarea height to fit its content
   adjustTextareaHeight(textarea);
 }
-function adjustTextareaHeight(textarea) { //fit textarea to content 
-  textarea.style.height = '0px';
-  textarea.style.height = textarea.scrollHeight + 'px';
-  //debuglog('adj');
+function adjustTextareaHeight(textarea, minRows=1, maxRows=Infinity) {
+  var t = textarea;
+  if (t.scrollTop == 0)
+    t.scrollTop=1;
+  while (t.scrollTop == 0) {
+    if (t.rows > minRows)
+      t.rows--;
+    else
+      break;
+    t.scrollTop = 1;
+    if (t.rows < maxRows)
+      t.style.overflowY = "hidden";
+    if (t.scrollTop > 0) {
+      t.rows++;
+      break;
+    }
+  }
+  while(t.scrollTop > 0) {
+    if (t.rows < maxRows) {
+      t.rows++;
+      if (t.scrollTop == 0)
+        t.scrollTop=1;
+    }
+    else {
+      t.style.overflowY = "auto";
+      break;
+    }
+  }
+  //https://stackoverflow.com/questions/17772260/textarea-auto-height
+  //debuglog(textarea.style.height);
+  /*textarea.style.height = (Number(textarea.style.height.replace('px', ''))-1)+'px';
+  textarea.style.height = (textarea.scrollHeight + 20) + "px";*/
+  /*
+  // Create a hidden "shadow" div with the same width as the textarea
+  var shadow = document.createElement('div');
+  shadow.style.width = window.getComputedStyle(textarea).width;
+  shadow.style.whiteSpace = 'pre-wrap';
+  shadow.style.wordWrap = 'break-word';
+  shadow.style.position = 'absolute';
+  //shadow.style.visibility = 'hidden';
+  shadow.style.height = 'auto';
+  shadow.style.overflow = 'hidden';
+
+  // Copy the textarea's value and styles to the shadow div
+  shadow.textContent = textarea.value;
+  shadow.style.padding = window.getComputedStyle(textarea).padding;
+  shadow.style.border = window.getComputedStyle(textarea).border;
+  shadow.style.fontSize = window.getComputedStyle(textarea).fontSize;
+  shadow.style.lineHeight = window.getComputedStyle(textarea).lineHeight;
+  shadow.style.letterSpacing = window.getComputedStyle(textarea).letterSpacing;
+
+  // Append the shadow div to the body (it won't be visible because it's absolutely positioned and has visibility: hidden)
+  document.body.appendChild(shadow);
+
+  // Calculate the required textarea height based on the shadow div's scrollHeight
+  var newHeight = shadow.scrollHeight;
+
+  // Remove the shadow div from the body
+  document.body.removeChild(shadow);
+
+  // Set the textarea's height
+  textarea.style.height = newHeight + 'px';
+  */
 }
 function debounceAndThrottle(func, debounceDelay, throttleDelay) {
   var timeoutId;
@@ -849,6 +908,16 @@ pet<(dog cat)
 dog=canines=dogs=doggy
 animal=animals<pet
 pet~"single creature subserviant to another (usually different species) for companionship of the master"
+||lorem|
+What is Lorem Ipsum?
+Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+Why do we use it?
+It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+Where does it come from?
+Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
+The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+Where can I get some?
+There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.
 ||pet dog cat|bowls
 ||pet dog|bark
 ||pet cat|meow
